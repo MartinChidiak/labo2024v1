8638cbfc1ebf12bdc4812c9b608dbc53760cf1df
MartinChidiak <mjchidiak@mail.austral.edu.ar>
2024-04-03 12:41:07 -0300

    Clase2Laptop

--- src/rpart/101_PrimerModelo.R
+++ src/rpart/101_PrimerModelo.R
@@ -7,7 +7,7 @@ require("rpart")
 require("rpart.plot")
 
 # Aqui se debe poner la carpeta de la materia de SU computadora local
-setwd("X:\\gdrive\\labo2024v1\\") # Establezco el Working Directory
+setwd("C:/Users/Martin/OneDrive/Maestría/8-LaboratoriodeImplementacion1/labo2024v1//") # Establezco el Working Directory
 
 # cargo el dataset
 dataset <- fread("./datasets/dataset_pequeno.csv")
@@ -21,9 +21,9 @@ modelo <- rpart(
         formula = "clase_ternaria ~ .",
         data = dtrain, # los datos donde voy a entrenar
         xval = 0,
-        cp = -0.3, # esto significa no limitar la complejidad de los splits
-        minsplit = 400, # minima cantidad de registros para que se haga el split
-        minbucket = 300, # tamaño minimo de una hoja
+        cp = -0.5, # esto significa no limitar la complejidad de los splits
+        minsplit = 320, # minima cantidad de registros para que se haga el split
+        minbucket = 16, # tamaño minimo de una hoja
         maxdepth = 6
 ) # profundidad maxima del arbol
 
@@ -60,6 +60,6 @@ dir.create("./exp/KA2001")
 
 # solo los campos para Kaggle
 fwrite(dapply[, list(numero_de_cliente, Predicted)],
-        file = "./exp/KA2001/K101_001.csv",
+        file = "./exp/KA2001/K101_001Seed6.csv",
         sep = ","
 )
