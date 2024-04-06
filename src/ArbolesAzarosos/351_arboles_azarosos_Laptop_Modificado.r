d742c5df6d446c871a522a001fdb175fc8c505db
MartinChidiak <mjchidiak@mail.austral.edu.ar>
2024-04-06 17:39:52 -0300

    LaptopTarea3

---
+++ src/ArbolesAzarosos/351_arboles_azarosos_Laptop_Modificado.r
@@ -0,0 +1,160 @@
+# Ensemble de arboles de decision
+# utilizando el naif metodo de Arboles Azarosos
+# entreno cada arbol utilizando un subset distinto de atributos del dataset
+
+# limpio la memoria
+rm(list = ls()) # Borro todos los objetos
+gc() # Garbage Collection
+
+require("data.table")
+require("rpart")
+
+# parmatros experimento
+PARAM <- list()
+PARAM$experimento <- 3510
+
+#en mi caso debo correr las siguientes 7 combinaciones de parametros
+# 1) cp = -1, minsplit = 100, minbucket = 5, maxdepth = 6, num_trees_max = 500
+# 2) cp = -1, minsplit = 250, minbucket = 50, maxdepth = 10, num_trees_max = 500
+# 3) cp = -1, minsplit = 500, minbucket = 100, maxdepth = 4, num_trees_max = 500
+# 4) cp = -1, minsplit = 750, minbucket = 200, maxdepth = 8, num_trees_max = 500
+# 5) cp = -1, minsplit = 1000, minbucket = 500, maxdepth = 14, num_trees_max = 500
+# 6) cp = -1, minsplit = 1500, minbucket = 200, maxdepth = 6, num_trees_max = 500
+# 7) cp = -1, minsplit = 2000, minbucket = 200, maxdepth = 10, num_trees_max = 500
+
+param_sets <- list(
+  list(minsplit = 100, minbucket = 5, maxdepth = 6),
+  list(minsplit = 250, minbucket = 50, maxdepth = 10),
+  list(minsplit = 500, minbucket = 100, maxdepth = 4),
+  list(minsplit = 750, minbucket = 200, maxdepth = 8),
+  list(minsplit = 1000, minbucket = 500, maxdepth = 14),
+  list(minsplit = 1500, minbucket = 200, maxdepth = 6),
+  list(minsplit = 2000, minbucket = 200, maxdepth = 10)
+)
+
+for (i in seq_along(param_sets)) {
+  # Set parameters
+  varminsplit <- param_sets[[i]]$varminsplit
+  varminbucket <- param_sets[[i]]$varminbucket
+  varmaxdepth <- param_sets[[i]]$varmaxdepth
+
+
+# parameetros rpart
+PARAM$rpart_param <- list(
+  "cp" = -1,
+  "minsplit" = varminsplit,
+  "minbucket" = varminbucket,
+  "maxdepth" = varmaxdepth
+)
+
+# parametros  arbol
+# entreno cada arbol con solo 50% de las variables variables
+PARAM$feature_fraction <- 0.5
+# voy a generar 500 arboles,
+#  a mas arboles mas tiempo de proceso y MEJOR MODELO,
+#  pero ganancias marginales
+PARAM$num_trees_max <- 5
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+# Aqui comienza el programa
+
+setwd("C:/Users/Martin/OneDrive/Maestría/8-LaboratoriodeImplementacion1/labo2024v1/") # Establezco el Working Directory
+
+#cargo MI semilla, que esta en MI bucket
+tabla_semillas <- fread( "./datasets//SemillasLaptop.txt" )
+ksemilla_azar <- tabla_semillas[ 1, semilla ]  # 1 es mi primer semilla
+
+# cargo los datos
+dataset <- fread("./datasets/dataset_pequeno.csv")
+
+
+# creo la carpeta donde va el experimento
+dir.create("./exp/", showWarnings = FALSE)
+carpeta_experimento <- paste0("./exp/KA", PARAM$experimento, "/")
+dir.create(paste0("./exp/KA", PARAM$experimento, "/"),
+  showWarnings = FALSE
+)
+
+setwd(carpeta_experimento)
+
+
+# que tamanos de ensemble grabo a disco, pero siempre debo generar los 500
+#grabar <- c(1, 5, 10, 50, 100, 200, 500)
+grabar <- c(1, 5)
+
+# defino los dataset de entrenamiento y aplicacion
+dtrain <- dataset[foto_mes == 202107]
+dapply <- dataset[foto_mes == 202109]
+
+# arreglo clase_ternaria por algun distraido ""
+dapply[, clase_ternaria := NA ]
+
+# elimino lo que ya no utilizo
+rm(dataset)
+gc()
+
+# aqui se va acumulando la probabilidad del ensemble
+dapply[, prob_acumulada := 0]
+
+# Establezco cuales son los campos que puedo usar para la prediccion
+# el copy() es por la Lazy Evaluation
+campos_buenos <- copy(setdiff(colnames(dtrain), c("clase_ternaria")))
+
+
+
+# Genero las salidas
+set.seed(ksemilla_azar) # Establezco la semilla aleatoria
+
+for (arbolito in 1:PARAM$num_trees_max) {
+  qty_campos_a_utilizar <- as.integer(length(campos_buenos)
+  * PARAM$feature_fraction)
+
+  campos_random <- sample(campos_buenos, qty_campos_a_utilizar)
+
+  # paso de un vector a un string con los elementos
+  # separados por un signo de "+"
+  # este hace falta para la formula
+  campos_random <- paste(campos_random, collapse = " + ")
+
+  # armo la formula para rpart
+  formulita <- paste0("clase_ternaria ~ ", campos_random)
+
+  # genero el arbol de decision
+  modelo <- rpart(formulita,
+    data = dtrain,
+    xval = 0,
+    control = PARAM$rpart_param
+  )
+
+  # aplico el modelo a los datos que no tienen clase
+  prediccion <- predict(modelo, dapply, type = "prob")
+
+  dapply[, prob_acumulada := prob_acumulada + prediccion[, "BAJA+2"]]
+
+  if (arbolito %in% grabar) {
+    # Genero la entrega para Kaggle
+    umbral_corte <- (1 / 40) * arbolito
+    entrega <- as.data.table(list(
+      "numero_de_cliente" = dapply[, numero_de_cliente],
+      "Predicted" = as.numeric(dapply[, prob_acumulada] > umbral_corte)
+    )) # genero la salida
+
+    nom_arch <- paste0(
+      "KA", PARAM$experimento, "_",
+      sprintf("%.3d", i),"_",
+      sprintf("%.3d", arbolito),  # para que tenga ceros adelante
+      ".csv"
+    )
+    fwrite(entrega,
+      file = nom_arch,
+      sep = ","
+    )
+
+    cat(arbolito, " ")
+  }
+}
+
+
+}
+
