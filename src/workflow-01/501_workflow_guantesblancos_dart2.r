33fb51c8eb974e262379ce0ca8bbf2985024acb0
MartinChidiak <mjchidiak@mail.austral.edu.ar>
2024-04-25 14:55:22 -0300

    vmf3 corr

--- src/workflow-01/501_workflow_guantesblancos_dart2.r
+++ src/workflow-01/501_workflow_guantesblancos_dart2.r
@@ -14,11 +14,11 @@ require("ParamHelpers")
 envg <- env()
 
 envg$EXPENV <- list()
-envg$EXPENV$exp_dir <- "~/buckets/b1/exp1/"
+envg$EXPENV$exp_dir <- "~/buckets/b1/exp2/"
 dir.create(envg$EXPENV$exp_dir, showWarnings = FALSE)
-envg$EXPENV$wf_dir <- "~/buckets/b1/flow1/"
+envg$EXPENV$wf_dir <- "~/buckets/b1/flow2/"
 dir.create(envg$EXPENV$wf_dir, showWarnings = FALSE)
-envg$EXPENV$wf_dir_local <- "~/flow1/"
+envg$EXPENV$wf_dir_local <- "~/flow2/"
 envg$EXPENV$repo_dir <- "~/labo2024v1/"
 envg$EXPENV$datasets_dir <- "~/buckets/b1/datasets/"
 envg$EXPENV$arch_sem <- "mis_semillas.txt"
